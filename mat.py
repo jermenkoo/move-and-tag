929159add5fe76241684834dc1fdf2b68ea949fe
George Pîrlea <george@dranov.ro>
2017-02-20 11:26:15 +0000

    Initial project skeleton

---
+++ mat.py
@@ -0,0 +1,11 @@
+import mat
+
+if __name__ == '__main__':
+    inFile = open('robots.mat', 'r')
+    outFile = open('out.mat', 'rw')
+
+    for line in inFile:
+        pass
+
+    inFile.close()
+    outFile.close()
---
+++ mat/world.py
@@ -0,0 +1,9 @@
+from mat import robot
+from mat import obstacle
+
+class World:
+    robots = []
+    obstacles = []
+
+    def __init__(self, line):
+        pass
