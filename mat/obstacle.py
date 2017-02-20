0168b7cd52fa2ea0e0dbdf9fcea7bdd984cf19ea
jasko1212si <jas.semrl@gmail.com>
2017-02-20 12:41:10 +0000

    Basic Obstacle Detection

--- mat/obstacle.py
+++ mat/obstacle.py
@@ -1,2 +1,14 @@
-class Obstacle:
-    pass
+from shapely.geometry import LineString, Polygon
+
+class Obstacle(Polygon):
+
+    #id    : int
+    #coord : [(int, int)]
+
+    def __init__(self, id, coordinates):
+        self.id = id
+        super().__init__(coordinates)
+
+    def intersect(self, tup1, tup2):
+        path = LineString([tup1, tup2])
+        path.intersects(super())
