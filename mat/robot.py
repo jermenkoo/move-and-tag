5afa7c37a9d62dae4777c4ca20527190f5150b10
Alexis Enston <xn@outlook.com>
2017-02-20 12:09:20 +0000

    robot class

--- mat/robot.py
+++ mat/robot.py
@@ -1,2 +1,24 @@
 class Robot:
-    pass
+    myCoord = ()
+    alive = False
+    myID = 0
+    myPath = []
+    
+    def __init__(self, mID, xyCoord):
+        self.myCoord = xyCoord
+        self.myID = mID
+        self.myPath.append(xyCoord)
+        if mID == 0:
+            self.alive = isAlive
+    
+    def goto(xyCoord):
+        self.myPath.append(xyCoord)
+        self.myCoord = xyCoord
+        
+        
+    
+        
+    
+        
+        
+    
