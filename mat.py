41b7ddb302cc598e309760f7b147089afe9699f6
Alexis Enston <xn@outlook.com>
2017-02-22 17:28:13 +0000

    multithreading

--- mat.py
+++ mat.py
@@ -12,6 +12,7 @@ def solveLine(line):
     world = World(line)
     world.visibilityGraph()
     G = world.graph()
+    pickle.dump(G, open('./graphs/' + world.id + '.graph', 'wb'))
     world.AGraphSolve(G)
     print (time.clock() - t0)
     return world.solution()
@@ -24,27 +25,27 @@ if __name__ == '__main__':
     outFile.write("lindworm\nf2k19t9k1o1o7i34q7o06jbph6\n")
 
     result = None
-    '''with Pool(8) as p:
+    with Pool(8) as p:
         result = p.map(solveLine, inFile.readlines())
         for line in result:
-            outFile.write(line + '\n')'''
+            outFile.write(line + '\n')
 
 
-    for line in inFile:
+    '''for line in inFile:
         t0 = time.clock()
         world = World(line)
-        world.visibilityGraph()
-        G = world.graph()
+        #world.visibilityGraph()
+        #G = world.graph()
         
-        pickle.dump(G, open('./graphs/' + world.id + '.graph', 'wb'))
+        #pickle.dump(G, open('./graphs/' + world.id + '.graph', 'wb'))
         
         
-        #G = pickle.load(open('./graphs/' + world.id + '.graph', 'rb'))
+        G = pickle.load(open('./graphs/' + world.id + '.graph', 'rb'))
         
         print(G.edges(data='weight'))
-        #world.AGraphSolve(G)
+        world.AGraphSolve(G)
         print (time.clock() - t0)
-        #outFile.write(world.solution() + '\n')
+        outFile.write(world.solution() + '\n')'''
 
     inFile.close()
     outFile.close()
